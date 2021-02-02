2b8a054f16cfcd9c4f4bd3ce10d87e1aa1a47e52
aGutierrezSacristan <a.gutierrez.sacristan@gmail.com>
2021-02-02 15:11:39 -0500

    summary table updated to take into account obfuscation

---
+++ R/summaryTableOne.R
@@ -0,0 +1,21 @@
+summaryTableOne <- function( df, obfuscation, stratifyBy ){
+  
+  df$subset <- factor( df$subset, levels=c("Subset Yes", "Subset No"), labels= c(stratifyBy, paste0( "No ",stratifyBy)))
+  table_one_vars <- c("sex","age_group")
+  
+  if( stratifyBy != "PheWAS code"){
+    table_one <- tableone::CreateTableOne(data=demographics,vars=table_one_vars,strata="subset")
+  }else{
+    table_one <- tableone::CreateTableOne(data=demographics,vars=table_one_vars)
+    
+  }
+  
+  if( obfuscation == FALSE ){
+    export_table_one <- print(table_one,showAllLevels=TRUE,formatOptions=list(big.mark=","), 
+                              test = FALSE)
+    }else{
+      export_table_one <- print(table_one,showAllLevels=TRUE,formatOptions=list(big.mark=","), 
+                                test = FALSE, format = "p")
+  }
+  return( export_table_one )
+}

e956cbb424dc6baa6f87f79b90343ae2691fe9a1
aGutierrezSacristan <a.gutierrez.sacristan@gmail.com>
2021-02-23 19:58:51 -0500

    updates done to do the analysis monthly, only in children (<25 years) and generating a RData file for the subsequent metanalysis

---
+++ R/dataFormat.R
@@ -0,0 +1,120 @@
+tempVariability <- function( VarSelection, obfuscation, observations, clinical, demographics, period, absoluteValue ){
+
+  if( VarSelection == "mental"){
+    colnames( mentalDisorder )[2] <- "description"
+    observations <- observations %>%
+      filter( concept_type == 'DIAG-ICD10', days_since_admission >= 0, concept_code %in% mentalDisorder$ICD10) %>%
+      mutate( pair = as.character(paste0( patient_num, "*",days_since_admission))) %>%
+      select( patient_num, pair, concept_code )
+  }else if( VarSelection == "autoimmune"){
+    colnames( autoimmune )[2] <- "description"
+    observations <- observations %>%
+      filter( concept_type == 'DIAG-ICD10', days_since_admission >= 0, concept_code %in% autoimmune$ICD10) %>%
+      mutate( pair = as.character(paste0( patient_num, "*",days_since_admission))) %>%
+      select( patient_num, pair, concept_code )
+  }else if( VarSelection == "phecodes"){
+    observations <- observations %>%
+      filter( concept_type == 'DIAG-ICD10', days_since_admission >= 0) %>%
+      mutate( pair = as.character(paste0( patient_num, "*",days_since_admission))) %>%
+      select( patient_num, pair, concept_code )
+  }else if( VarSelection == "medication"){
+    observations <- observations %>%
+      filter( concept_type == 'MED-CLASS', days_since_admission >= 0) %>%
+      mutate( pair = as.character(paste0( patient_num, "*",days_since_admission))) %>%
+      select( patient_num, pair, concept_code )
+  }
+  
+  observations$concept_code <- as.character( observations$concept_code)
+  
+  clinical <- clinical %>%
+    mutate( pair = as.character(paste0( patient_num, "*",days_since_admission))) %>%
+    select( patient_num, pair, calendar_date, severe, deceased)
+  
+  final <- merge( observations, clinical, by="pair") 
+  
+  final <- final %>%
+    mutate(patient_num = patient_num.x) %>%
+    select(patient_num, concept_code, calendar_date, severe )
+  
+  demographics <- unique( demographics %>%
+                            select( patient_num, age_group, sex ) )
+  
+  demographics$subset <- ifelse( demographics$patient_num %in% final$patient_num, "Subset Yes", "Subset No")
+  
+  ## Create the summary table 1
+  tbl1 <- summaryTableOne( df = demographics, obfuscation = obfuscation, stratifyBy = VarSelection)
+  
+  final <- EHRtemporalVariability::formatDate(
+    input         = final,
+    dateColumn    = "calendar_date",
+    dateFormat = "%Y-%m-%d"
+  )
+  
+  ## Extract number of month or number of week
+  if(  period == "week"){
+    final$num  <- format( as.Date( final$calendar_date), "%U")
+    }else if(  period == "month"){
+      final$num  <- format( as.Date( final$calendar_date), "%m")
+    }
+
+  ###
+  final <- final %>% 
+    dplyr::group_by(patient_num, concept_code, num ) %>% 
+    dplyr::mutate( minDate = min(calendar_date)) %>%
+    dplyr::select( patient_num, concept_code, minDate, num ) %>%
+    unique()
+  
+  
+  
+  ## Remove the patient identifier
+  final <- final[, -1]
+
+  if( VarSelection != "medication"){
+  
+    if( VarSelection == "mental"){
+      final <- merge( final, mentalDisorder, by.x = "concept_code", by.y = "ICD10")
+    }else if( VarSelection ==  "autoimmune"){
+      final <- merge( final, autoimmune, by.x = "concept_code", by.y = "ICD10")
+    }else if( VarSelection == "phecodes"){
+      phecode <- read.csv("public-data/phecode_icd10.csv", colClasses = "character")
+      phecode$Phenotype <- ifelse( phecode$Phenotype == "", phecode$ICD10.String, phecode$Phenotype )
+      phecode <- unique( phecode %>%
+                           mutate( concept_code = ICD10, description = Phenotype ) %>%
+                           select( concept_code, PheCode, description ))
+      final <- merge( final, phecode)
+    }
+    
+    final$description <- as.character(final$description)
+    final$descriptionShort <- substr(final$description, 1, 40)
+  }  
+  
+  probMaps <- estimateDataTemporalMap(data           = final, 
+                                      dateColumnName = "minDate", 
+                                      period         = period)
+  if( obfuscation !=  FALSE ){
+    
+    for( i in 1:length(probMaps)){
+      probMaps[[i]]@countsMap[probMaps[[i]]@countsMap > 0 & probMaps[[i]]@countsMap <= obfuscation ] <- 0.5 * obfuscation
+    }
+  }
+  names(probMaps) <- paste0( VarSelection,names(probMaps))
+  
+  if( VarSelection != "medication"){
+    print( plotDataTemporalMap(
+      dataTemporalMap =  probMaps[[paste0( VarSelection,"descriptionShort")]],
+      startValue = 1,
+      endValue = 20,
+      colorPalette    = "Magma", 
+      absolute = absoluteValue))
+  }else{
+    print( plotDataTemporalMap(
+      dataTemporalMap =  probMaps[[paste0( VarSelection,"concept_code")]],
+      startValue = 1,
+      endValue = 20,
+      colorPalette    = "Magma", 
+      absolute = absoluteValue))
+  }
+  
+  return( probMaps)
+  
+}
--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 ## How to run this first approach?
 First, clone the repository: git clone https://github.com/aGutierrezSacristan/temporalVariabilityAnalysis
 
-Then open the file temporalVariability.Rmd and:
+Then open the file monthlytemporalVariability.Rmd and:
 - add your site ID to the title
 - change the myinputFiles to the directory where your phase 2.1 data is located
 - determine the obfuscation threshold: 
@@ -12,9 +12,9 @@ Then open the file temporalVariability.Rmd and:
     - the numeric value of the obfuscation threshold if any; e.g. obfuscation = 3
     Make sure you comment the existing obfuscation line, set up as FALSE
 - change the absoluteValue variable as TRUE or FALSE to indicate if you want to visualize in the heatmaps the counts (absoluteValue = TRUE), or the frequencies (absoluteValue = FALSE)
-  
 
-After all these changes are done, run the temporalVariability.Rmd, as an output an html file named: temporalVariability.html should be generated. 
+
+After all these changes are done, run the monthlytemporalVariability.Rmd, as an output an html file named: monthlytemporalVariability.html and a RData file named myExport_month.RData should be generated. 
 
 
 
